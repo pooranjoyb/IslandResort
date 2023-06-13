440282d581ff10f92d097009fd8ea9807031f275
Pooranjoy <pooranjoyb2016@gmail.com>
2023-06-13 19:56:13 +0530

    Successful port of ui to the backend

--- backend/src/main/java/com/islandresort/backend/BackendApplication.java
+++ backend/src/main/java/com/islandresort/backend/BackendApplication.java
@@ -1,13 +1,26 @@
 package com.islandresort.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class BackendApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	
+	@Controller
+	public static class HomeController {
+
+		@GetMapping(value = {"/", "/{path:[^\\.]*}"})
+		public String index() {
+			return "forward:/index.html";
+		}
+	}
+
 
 }
